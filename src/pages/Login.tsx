b7cef810ba26c8d601bd3648f7a351253b50bbbd
Kyu Sung Sohn <rok.ksohn@gmail.com>
2023-07-14 12:28:43 +0900

    Style: 로그인 페이지 초기 UI 구현

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,30 +3,13 @@ import { Link } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import styled from '@emotion/styled';
 
 function Login() {
 	return (
-		<Container
-			fixed
-			sx={{
-				width: '100%',
-				height: '100%',
-				display: 'flex',
-				alignItems: 'center',
-			}}
-		>
-			<Container
-				maxWidth="sm"
-				sx={{
-					border: '1px solid var(--border-color)',
-					borderRadius: '10px',
-					padding: '30px',
-					height: '700px',
-				}}
-			>
+		<Background fixed>
+			<LogInBox maxWidth="sm">
 				<LogoMessageWrapper>
 					<Logo src="/public/logo/royal-flash-logo.png" alt="로얄플래시 로고" />
 					<WelcomeMessage>
@@ -35,39 +18,61 @@ function Login() {
 				</LogoMessageWrapper>
 				<Box
 					component="form"
-					sx={{ '& > :not(style)': { m: 1, width: '100%' } }}
+					sx={{
+						'& > :not(style)': {
+							m: 1,
+							width: '100%',
+							marginTop: '20px',
+						},
+					}}
 					noValidate
 					autoComplete="off"
 				>
 					<TextField
 						required
-						id="standard-basic"
-						label="email"
+						id="email-input"
+						label="Email"
 						variant="standard"
 					/>
 					<TextField
 						required
-						id="standard-basic"
-						label="password"
+						id="password-input"
+						label="Password"
 						variant="standard"
 					/>
-					<Stack direction="row" spacing={2}>
-						<Button variant="outlined">로그인</Button>
-					</Stack>
 				</Box>
-				<Link to={'/signup'}>
-					<span>아직 회원이 아니신가요?</span>
-				</Link>
-			</Container>
-		</Container>
+				<LogInButtonBox>
+					<LogInButton variant="outlined">로그인</LogInButton>
+				</LogInButtonBox>
+				<RegisterLinkWrapper>
+					<RegisterLink to={'/signup'}>
+						<span>아직 회원이 아니신가요?</span>
+					</RegisterLink>
+				</RegisterLinkWrapper>
+			</LogInBox>
+		</Background>
 	);
 }
+
+const Background = styled(Container)`
+	width: 100%;
+	height: 100%;
+	display: flex;
+	align-items: center;
+`;
+
+const LogInBox = styled(Container)`
+	border: 1px solid var(--border-color);
+	border-radius: 10px;
+	padding: 30px;
+	height: 600px;
+`;
 
 const LogoMessageWrapper = styled.div`
 	display: flex;
 	flex-direction: column;
 	padding: 10px;
-	margin-bottom: 100px;
+	margin-bottom: 40px;
 	align-items: center;
 `;
 
@@ -82,6 +87,35 @@ const WelcomeMessage = styled.p`
 
 	> span {
 		font-weight: bold;
+	}
+`;
+
+const LogInButtonBox = styled(Box)`
+	text-align: center;
+`;
+
+const LogInButton = styled(Button)`
+	width: 150px;
+	margin-top: 30px;
+`;
+
+const RegisterLinkWrapper = styled(Box)`
+	width: 100%;
+	text-align: center;
+	margin-top: 10px;
+`;
+
+const RegisterLink = styled(Link)`
+	text-align: center;
+	color: gray;
+
+	> span {
+		font-size: 12px;
+		transition: 0.1s ease-in;
+
+		:hover {
+			color: var(--font-color);
+		}
 	}
 `;
 
