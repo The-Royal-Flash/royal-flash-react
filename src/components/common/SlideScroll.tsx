2b7f6833493ec74d5625c3301a92f2856f318950
sunhwa <sumz.ko@gmail.com>
2023-07-16 12:18:40 +0900

    Feat: 공통 Animation 컴포넌트

---
+++ src/components/common/SlideScroll.tsx
@@ -0,0 +1,33 @@
+import { motion } from 'framer-motion';
+import { Slide } from '@mui/material';
+import useCheckInView from '../../hooks/useCheckInView';
+import styled from '@emotion/styled';
+
+interface SlideScrollOnceProps {
+	children: React.ReactElement;
+}
+
+function SlideScrollOnce({ children }: SlideScrollOnceProps) {
+	const { ref, isInView } = useCheckInView(0.5);
+
+	return (
+		<Container
+			ref={ref}
+			initial={{ opacity: 0 }}
+			animate={isInView ? { opacity: 1 } : { opacity: 0 }}
+			transition={{ duration: 1 }}
+		>
+			<Slide in={isInView} direction="up" timeout={1000}>
+				{children}
+			</Slide>
+		</Container>
+	);
+}
+const Container = styled(motion.div)`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+export default SlideScrollOnce;
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,5 @@
 export { default as Modal } from './Modal';
 export { default as ImageButton } from './ImageButton';
 export { default as ExpandTooltipButton } from './ExpandTooltipButton';
+export { default as SlideScroll } from './SlideScroll';
+export { default as FadeIn } from './FadeIn';
