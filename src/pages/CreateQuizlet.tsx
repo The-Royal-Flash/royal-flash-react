80a316d45b98acc3ca8d5026237c039e4a8a8b7e
sunhwa <sumz.ko@gmail.com>
2023-07-19 13:49:42 +0900

    Feat: CreateQuizlet 페이지 - form

--- src/pages/CreateQuizlet.tsx
+++ src/pages/CreateQuizlet.tsx
@@ -1,25 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
-import { Button, Typography } from '@mui/material';
+import { Button, TextField, Typography } from '@mui/material';
 import { desktopMediaQuery, mobileMediaQuery } from '../utils/mediaQueries';
 import { QuestionCardForm, QuizletForm } from '../components/quizlet';
+import { Form, useNavigate } from 'react-router-dom';
+import { SubmitHandler, useFieldArray, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createQuizletSchema } from '../schemas/quizletSchema';
+import { CreateQuizletRequest } from '../types/quizlet';
 
 function CreateQuizlet() {
+	const navi = useNavigate();
+
+	const {
+		handleSubmit,
+		register,
+		control,
+		formState: { errors },
+	} = useForm<CreateQuizletRequest>({
+		resolver: zodResolver(createQuizletSchema),
+		defaultValues: {
+			title: '',
+			description: '',
+			tagList: [],
+			questionCardList: [{ question: '', link: '', answer: '' }],
+		},
+	});
+
+	const { fields, append, remove } = useFieldArray({
+		control,
+		name: 'questionCardList',
+	});
+
+	const onSubmitHandler: SubmitHandler<CreateQuizletRequest> = async (data) => {
+		const { title, description, tagList, questionCardList } = data;
+		console.log(title, description, tagList, questionCardList);
+	};
+
 	return (
 		<Container>
 			<Title>새로운 학습세트 만들기</Title>
-			<Form>
-				<QuizletForm />
-				<QuestionCardForm />
-			</Form>
-			<ButtonGroup>
-				<StyledButton variant="contained" type="submit">
-					학습세트 생성
-				</StyledButton>
-				<StyledButton variant="outlined" type="submit">
-					취소
-				</StyledButton>
-			</ButtonGroup>
+			<StyledForm onSubmit={handleSubmit(onSubmitHandler)}>
+				<QuizletForm register={register} control={control} errors={errors} />
+				{fields.map((_, index) => (
+					<QuestionCardForm
+						key={index}
+						index={index}
+						register={register}
+						errors={errors}
+						remove={remove}
+					/>
+				))}
+				<ButtonGroup>
+					<Button
+						type="button"
+						variant="contained"
+						onClick={() => append({ question: '', link: '', answer: '' })}
+					>
+						문제 추가
+					</Button>
+					<StyledButton type="submit" variant="contained">
+						학습세트 생성
+					</StyledButton>
+					<StyledButton type="button" variant="outlined">
+						취소
+					</StyledButton>
+				</ButtonGroup>
+			</StyledForm>
 		</Container>
 	);
 }
@@ -43,7 +90,7 @@ const Title = styled(Typography)`
 	}
 `;
 
-const Form = styled.form`
+const StyledForm = styled(Form)`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
