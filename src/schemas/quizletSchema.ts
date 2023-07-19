078cdd8e4fc30a7ce615479e73425768bf62ae04
sunhwa <sumz.ko@gmail.com>
2023-07-19 13:47:44 +0900

    -: createQuizletSchema 및 type 정의

--- src/schemas/quizletSchema.ts
+++ src/schemas/quizletSchema.ts
@@ -1,17 +1,18 @@
 import { z } from 'zod';
 
-const CreateQuizletSchema = z.object({
+const createQuizletSchema = z.object({
 	title: z.string().min(5, '5자 이상의 제목을 입력해 주세요.'),
 	description: z.string().min(10, '10자 이상의 설명을 입력해 주세요.'),
-	url: z.string(),
+	tagList: z.array(z.string()),
 	questionCardList: z
-		.object({
-			question: z.string().min(3, '3자 이상의 문제를 입력해 주세요.'),
-			answer: z.string().min(1, '모범 답안을 입력해 주세요.'),
-			link: z.string(),
-		})
-		.array()
-		.nonempty(),
+		.array(
+			z.object({
+				question: z.string().min(5, '5자 이상의 문제를 입력해 주세요.'),
+				answer: z.string().min(1, '모범 답안을 입력해 주세요.'),
+				link: z.string(),
+			}),
+		)
+		.min(1),
 });
 
-export { CreateQuizletSchema };
+export { createQuizletSchema };
--- src/types/quizlet.d.ts
+++ src/types/quizlet.d.ts
@@ -1,5 +1,8 @@
+import { z } from 'zod';
+import { createQuizletSchema } from '../schemas/quizletSchema';
+
 /** 학습세트 기본 정보 */
-interface BaseQuizletInfo {
+export interface BaseQuizletInfo {
 	/** 학습세트 제목 */
 	title: string;
 	/** 학습세트 설명 */
@@ -9,7 +12,7 @@ interface BaseQuizletInfo {
 }
 
 /** 암키 카드 */
-interface QuestionCard {
+export interface QuestionCard {
 	/** 문제 */
 	question: string;
 	/** 정답 */
@@ -19,7 +22,7 @@ interface QuestionCard {
 }
 
 /** 학습 기록이 있는 학습 세트의 기본 정보 */
-interface BaseMyQuizlet {
+export interface BaseMyQuizlet {
 	/** 학습 (완료) 횟수 */
 	studyCount: number;
 	/** 문제(질문) 수 */
@@ -31,7 +34,7 @@ interface BaseMyQuizlet {
 }
 
 /** 페이지네이션 필수 정보 */
-interface BasePagenation {
+export interface BasePagenation {
 	/** 현재 페이지 번호 */
 	page: number;
 	/** 전체 페이지 수 */
@@ -39,12 +42,19 @@ interface BasePagenation {
 }
 
 /** 학습 세트 정보 */
-interface QuizletResponse extends BaseQuizletInfo {
+export interface QuizletResponse extends BaseQuizletInfo {
 	questionCardList: Array<QuestionCard>;
 }
 
+/** 학습 세트 생성 */
+export type CreateQuizletRequest = z.infer<typeof createQuizletSchema>;
+
+export type CreateQuizletQuestionCard = z.infer<
+	typeof createQuizletSchema
+>['questionCardList'][number];
+
 /** 학습 세트 상세 정보  */
-interface QuizletDetailResponse
+export interface QuizletDetailResponse
 	extends BaseQuizletInfo,
 		Partial<BaseMyQuizlet> {
 	/** 암기 카드의 문제 목록 */
@@ -52,7 +62,7 @@ interface QuizletDetailResponse
 }
 
 /** 학습세트 검색 페이지에서 보여질 학습세트 정보 */
-interface SearchQuizlet extends QuizletInfo {
+export interface SearchQuizlet extends QuizletInfo {
 	/** 암기 카드 수 */
 	numOfCards: number;
 	/** 학습세트 작성자 */
@@ -62,7 +72,7 @@ interface SearchQuizlet extends QuizletInfo {
 }
 
 /** 나의 학습세트 검색 페이지에서 보여질 학습세트 정보 */
-interface MyQuizlet extends BaseMyQuizlet {
+export interface MyQuizlet extends BaseMyQuizlet {
 	/** 학습세트 제목 */
 	title: string;
 	/** 태그 목록 */
@@ -70,13 +80,13 @@ interface MyQuizlet extends BaseMyQuizlet {
 }
 
 /** 학습세트 검색 결과 - 학습세트 목록 */
-interface SearchResponse extends BasePagenation {
+export interface SearchResponse extends BasePagenation {
 	/** 학습세트 목록 검색 결과 */
 	quizletList: Array<SearchQuizlet>;
 }
 
 /** 나의 학습세트 검색 결과 - 학습세트 목록 */
-interface MyQuizletSearchResponse extends BasePagenation {
+export interface MyQuizletSearchResponse extends BasePagenation {
 	/** 나의 학습세트 목록 검색 결과  */
 	quizletList: Array<MyQuizlet>;
 }
