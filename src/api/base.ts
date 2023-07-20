d4cb278f1f4dfdb86e0111e19b6fcb2d9ca2284c
sunhwa <sumz.ko@gmail.com>
2023-07-20 09:28:59 +0900

    Server: base api

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,10 +1,20 @@
 import axios from 'axios';
+import qs from 'qs';
+
+const service = axios.create({
+	baseURL: import.meta.env.VITE_BASE_URL,
+	paramsSerializer: (params) => qs.stringify(params, { arrayFormat: 'comma' }),
+	timeout: 1000,
+});
 
 export const http = {
-	get: function get<T>(url: string): Promise<T> {
-		return axios.get(url).then((res) => res.data);
+	get: function get<T>(url: string, params?: any): Promise<T> {
+		return service.get(url, { params }).then((res) => res.data);
 	},
 	post: function post<T>(url: string, data: any): Promise<T> {
-		return axios.post(url, { ...data });
+		return service.post(url, { ...data });
+	},
+	delete: function remove<T>(url: string, data: any): Promise<T> {
+		return service.delete(url, { ...data });
 	},
 };
