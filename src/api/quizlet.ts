dd2f8aee4cf79eedd903574c15da43e7956c4f5f
sunhwa <sumz.ko@gmail.com>
2023-07-20 22:14:09 +0900

    Feat: api

--- src/api/quizlet.ts
+++ src/api/quizlet.ts
@@ -1,18 +1,26 @@
 import { http } from './base';
-import { CreateQuizletRequest } from '../types/quizlet';
+import { QuizletRequest } from '../types/quizlet';
 import { BaseApiResponse } from '../types/response';
 
 // 학습세트 생성
 export const createQuizlet = async (
-	quizletInfo: CreateQuizletRequest,
-): Promise<BaseApiResponse> =>
-	await http.post(`http://localhost:8080/api/quizlet/create`, quizletInfo);
-// await http.post(`/api/quizlet/create`, quizletInfo); // mock-server
+	quizletInfo: QuizletRequest,
+): Promise<BaseApiResponse> => await http.post('quizlet/create', quizletInfo);
+
+// 학습세트 정보
+export const fetchQuizlet = async (quizletId: string) =>
+	await http.get<BaseApiResponse>(`quizlet/info/${quizletId}`);
 
 // 학습세트 수정
+export const editQuizlet = async (
+	quizletId: string,
+	quizletInfo: QuizletRequest,
+) => await http.post<BaseApiResponse>(`quizlet/edit/${quizletId}`, quizletInfo);
 
 // 학습세트 삭제
+export const deleteQuizlet = async (quizletId: string) =>
+	await http.get<BaseApiResponse>(`quizlet/detail/${quizletId}`);
 
 // 학습세트 상세정보
 export const fetchQuizletDetail = async (quizletId: string) =>
-	await http.get(`/api/quizlet/detail/${quizletId}`); // mock-server
+	await http.get<BaseApiResponse>(`quizlet/detail/${quizletId}`);
