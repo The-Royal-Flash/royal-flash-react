150be4ed8d0abe13a34adb92db8d7573913f575c
sunhwa <sumz.ko@gmail.com>
2023-07-22 21:52:46 +0900

    Refac: quizlet schema 리팩토링

--- src/schemas/quizletSchema.ts
+++ src/schemas/quizletSchema.ts
@@ -7,27 +7,24 @@ export const baseQuizletSchema = z.object({
 	tagList: z.array(z.string()),
 });
 
+/** 학습 문제 스키마 */
+const quizletCardSchema = z.object({
+	question: z.string().min(3, '3자 이상의 문제를 입력해 주세요.'),
+	answer: z.string().min(1, '모범 답안을 입력해 주세요.'),
+	link: z
+		.string()
+		.url('http 또는 https로 시작하는 url 주소를 입력해주세요.')
+		.optional()
+		.or(z.literal('')),
+});
+
 /** 학습세트 생성 스키마 */
 export const createQuizletSchema = baseQuizletSchema.extend({
-	questionCardList: z
-		.array(
-			z.object({
-				question: z.string().min(3, '3자 이상의 문제를 입력해 주세요.'),
-				answer: z.string().min(1, '모범 답안을 입력해 주세요.'),
-				link: z.string(),
-			}),
-		)
-		.min(1),
+	questionCardList: z.array(quizletCardSchema).min(1),
 });
 
 /** 학습세트 수정 스키마 */
 export const editQuizletSchema = baseQuizletSchema.extend({
 	questionListToRemove: z.array(z.string()),
-	questionCardListToAdd: z.array(
-		z.object({
-			question: z.string().min(3, '3자 이상의 문제를 입력해 주세요.'),
-			answer: z.string().min(1, '모범 답안을 입력해 주세요.'),
-			link: z.string(),
-		}),
-	),
+	questionCardListToAdd: z.array(quizletCardSchema),
 });
