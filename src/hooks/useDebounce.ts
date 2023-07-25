9f16030e5449ec687757f916cfe2ed6151d9d86b
Kyu Sung Sohn <rok.ksohn@gmail.com>
2023-07-25 10:53:38 +0900

    Feat: useDebounce 커스텀 훅 추가

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,7 @@
+import { useCallback } from 'react';
+import { debounce } from 'lodash';
+
+const useDebounce = (callback: (...args: any) => any, ms: number) =>
+	useCallback(debounce(callback, ms), []);
+
+export default useDebounce;
