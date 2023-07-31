44c1b26609ce4e85a03455d3a781999f2706cb74
Kyu Sung Sohn <rok.ksohn@gmail.com>
2023-07-31 11:30:01 +0900

    -: mediaQueries.ts 파일 복구

---
+++ src/mediaQueries.ts
@@ -0,0 +1,5 @@
+export const minWidth = 370;
+export const mobileWidth = 640;
+export const maxWidth = 1280;
+export const mobileMediaQuery = `@media (max-width: ${mobileWidth}px)`;
+export const desktopMediaQuery = `@media (min-width: ${mobileWidth + 1}px)`;
