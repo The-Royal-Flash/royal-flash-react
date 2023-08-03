c34ba40cc09226e85a58f976382546274dfe66ff
sunhwa <sumz.ko@gmail.com>
2023-08-03 20:59:39 +0900

    Fix: providers 디렉터리 생성 (이동)

--- src/components/auth/index.ts
+++ src/components/auth/index.ts
@@ -4,5 +4,4 @@ export { default as InterchangeButton } from './InterchangeButton';
 export { default as LoginForm } from './LoginForm';
 export { default as SignupForm } from './SignupForm';
 export { default as FormInput } from './FormInput';
-export { default as UserProvider } from './UserProvider';
 export { default as AuthenticationGuard } from './AuthenticationGuard';
