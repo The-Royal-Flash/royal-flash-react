664bd42bbf79ecd07f8c3eff137a0bf91d6fc807
Kyu Sung Sohn <rok.ksohn@gmail.com>
2023-08-09 11:19:19 +0900

    Fix: 하단 스크롤바 오류 수정

--- src/components/study/Card.tsx
+++ src/components/study/Card.tsx
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 import { desktopMediaQuery, mobileMediaQuery } from '../../utils/mediaQueries';
 import { css } from '@mui/material';
 import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import { MIN_SWIPE_DISTANCE } from '../../constants';
 import {
 	GhostCard,
 	EmptyCard,
@@ -11,7 +12,6 @@ import {
 	QuestionCard,
 	FinishedCard,
 } from '.';
-import { MIN_SWIPE_DISTANCE } from '../../constants';
 
 interface CardProps {
 	goToNextCard: (isWrong: boolean, _id: string) => void;
@@ -169,7 +169,7 @@ function Card({
 }
 
 const Container = styled.div`
-	width: 100vw;
+	width: 90vw;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
